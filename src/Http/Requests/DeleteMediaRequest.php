077e9cce4cc887511d9dba47b6bad5b72570cb2f
DenTray <pankratov.denis@mail.ru>
2024-04-03 11:07:24 +0600

    Update src/Http/Requests/DeleteMediaRequest.php

--- src/Http/Requests/DeleteMediaRequest.php
+++ src/Http/Requests/DeleteMediaRequest.php
@@ -18,7 +18,7 @@ class DeleteMediaRequest extends Request implements DeleteMediaRequestContract
 
     public function validateResolved(): void
     {
-        $this->media = app(MediaServiceContract::class)->first($this->route('id'))->first();
+        $this->media = app(MediaServiceContract::class)->first($this->route('id'));
 
         if (empty($this->media)) {
             throw new NotFoundHttpException(__('validation.exceptions.not_found', ['entity' => 'Media']));
