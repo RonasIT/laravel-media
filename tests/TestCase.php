5dbf83b2b5216867dfcc8f76bc6863ab011f1646
Evgeny Ulin <eulin@ronasit.com>
2024-06-02 12:55:19 +0300

    #18: Added inclusion of .env files in tests.
    Also corrected fixtures so that tests would pass.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,9 @@
 namespace RonasIT\Media\Tests;
 
 use Carbon\Carbon;
+use Dotenv\Dotenv;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\File;
 use Orchestra\Testbench\TestCase as BaseTest;
 use RonasIT\Media\MediaServiceProvider;
 use RonasIT\Support\Traits\FixturesTrait;
@@ -43,6 +45,30 @@ class TestCase extends BaseTest
 
     protected function getEnvironmentSetUp($app): void
     {
+        $this->includeEnv();
+
+        $this->setupDb($app);
+    }
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            MediaServiceProvider::class,
+        ];
+    }
+
+    protected function includeEnv(): void
+    {
+        $pathToEnv = str_replace('/vendor/orchestra/testbench-core/laravel', '',  base_path());
+        $name = env('APP_ENV') ? '.env.' . env('APP_ENV') : null;
+        if (File::exists("{$pathToEnv}/{$name}")) {
+            $dotenv = Dotenv::createImmutable($pathToEnv, $name);
+            $dotenv->load();
+        }
+    }
+
+    protected function setupDb($app): void
+    {
         $app['config']->set('database.default', env('DB_DEFAULT', 'pgsql'));
         $app['config']->set('database.connections.pgsql', [
             'driver' => env('DB_DRIVER', 'pgsql'),
@@ -50,12 +76,5 @@ class TestCase extends BaseTest
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
         ]);
-    }
-
-    protected function getPackageProviders($app): array
-    {
-        return [
-            MediaServiceProvider::class
-        ];
     }
 }
