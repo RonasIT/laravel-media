986dd134800d59a6fddbe5a541d5dc8243e8347c
DenTray <dpankratov@ronasit.com>
2024-11-21 16:43:32 +0600

    Apply suggestions from code review

--- src/Enums/MediaRouteActionEnum.php
+++ src/Enums/MediaRouteActionEnum.php
@@ -5,10 +5,7 @@ namespace RonasIT\Media\Enums;
 enum MediaRouteActionEnum: string
 {
     case SingleUpload = 'create';
-
     case Delete = 'delete';
-
     case Search = 'search';
-
     case BulkCreate = 'bulk_create';
 }
