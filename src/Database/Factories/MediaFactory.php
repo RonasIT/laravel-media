d045812de108333101fc08b3fb6c731e3d77a993
Ruslan Guskov <feniksuh@gmail.com>
2024-12-02 17:30:07 +0300

    feat:explicit decare factory, buld:remove seeder

--- src/Database/Factories/MediaFactory.php
+++ src/Database/Factories/MediaFactory.php
@@ -1,13 +1,16 @@
 <?php
 
-namespace RonasIT\Media\database\factories;
+namespace RonasIT\Media\Database\Factories;
 
 use Faker\Generator as Faker;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Storage;
+use RonasIT\Media\Models\Media;
 
 class MediaFactory extends Factory
 {
+    protected $model = Media::class;
+
     public function definition(): array
     {
         $faker = app(Faker::class);
