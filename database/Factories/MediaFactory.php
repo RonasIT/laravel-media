5be498f432d022c2e7e7f989c7b480576770105b
Ruslan Guskov <feniksuh@gmail.com>
2024-12-02 17:04:32 +0300

    feat:media factory

---
+++ database/Factories/MediaFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Factories;
+
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+
+class MediaFactory extends Factory
+{
+    public function definition(): array
+    {
+        $faker = app(Faker::class);
+
+        $name = $faker->unique()->lexify('???????????????'). 'jpg';
+
+        return [
+            'name' => $name,
+            'link' => Storage::url($name),
+        ];
+    }
+}
