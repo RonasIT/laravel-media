7003733dfcaec1df834a37e3f6ee3b5cea9da3ee
roman <roman2027@internet.ru>
2024-12-04 19:20:52 +0600

    feat: add preview to response refs: https://github.com/RonasIT/laravel-media/issues/33

--- src/Http/Resources/MediaResource.php
+++ src/Http/Resources/MediaResource.php
@@ -14,6 +14,7 @@ class MediaResource extends BaseResource implements MediaResourceContract
             'name' => $this->resource->name,
             'is_public' => $this->resource->is_public,
             'meta' => $this->resource->meta,
+            'preview' => MediaResource::make($this->whenLoaded('preview')),
         ];
     }
 }
