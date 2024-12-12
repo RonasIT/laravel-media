9d75e5b167ace489076bba8e824cd7f369889a6a
Ni Nelli <nellini@ronasit.com>
2024-12-12 14:37:10 +0900

    refactor: move max upload file size to config

--- config/media.php
+++ config/media.php
@@ -3,6 +3,14 @@
 use App\Models\User;
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Permitted file types
+    |--------------------------------------------------------------------------
+    |
+    | The list of permitted file types which will be validated
+    */
+
     'permitted_types' => [
         'jpg',
         'jpeg',
@@ -10,12 +18,36 @@ return [
         'png',
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Classes
+    |--------------------------------------------------------------------------
+    |
+    | Authenticatable User model which will be used to create owner relation with Media
+    */
+
     'classes' => [
         'user_model' => User::class,
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Preview
+    |--------------------------------------------------------------------------
+    |
+    | File preview width and height in pixels which will be used to create preview
+    */
     'preview' => [
         'width' => 250,
         'height' => 250,
     ],
+
+    /*
+   |--------------------------------------------------------------------------
+   | Max File Size
+   |--------------------------------------------------------------------------
+   |
+   | Max File size in kilobytes which will be validated
+   */
+    'max_file_size' => 5120,
 ];
